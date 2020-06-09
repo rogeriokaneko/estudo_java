947c4eac3232f6f5282af6ba842a83dd25509bc6
Rogério Kiyoshi Kaneko <rogerio.kaneko@gmail.com>
2020-06-08 21:26:04 -0300

    Exemplos da terceira aula.

---
+++ src/br/com/digitalinnovation/one/ParadigmaFuncional/ExemploAulaTres.java
@@ -0,0 +1,39 @@
+package br.com.digitalinnovation.one.ParadigmaFuncional;
+
+public class ExemploAulaTres {
+
+    public static void main(String[] args) {
+
+        //
+        Funcao1 colocarPrefixoSenhorNaString1 = new Funcao1(){
+            @Override
+            public String gerar(String valor) {
+                return "Sr. " + valor;
+            }
+        };
+
+        Funcao1 colocarPrefixoSenhorNaString = valor -> "Sr. " + valor;
+
+        System.out.println(colocarPrefixoSenhorNaString1.gerar("teste"));
+        System.out.println(colocarPrefixoSenhorNaString.gerar("teste"));
+
+        System.out.println("************************************************");
+
+        Funcao2 funcao2 = valor -> {
+            System.out.println(valor);
+            System.out.println(valor);
+        };
+        funcao2.gerar("Rogerio");
+    }
+
+}
+
+@FunctionalInterface
+interface Funcao1{
+    String gerar(String valor);
+}
+
+@FunctionalInterface
+interface Funcao2{
+    void gerar(String valor);
+}
