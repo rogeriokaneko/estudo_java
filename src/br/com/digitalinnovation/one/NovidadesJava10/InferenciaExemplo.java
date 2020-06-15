267afdc94f02ac0395bef4058e6ee9a85f82d473
Rogério Kiyoshi Kaneko <rogerio.kaneko@gmail.com>
2020-06-14 23:05:56 -0300

    Commit relacionada à aula "Aplicando os novos releases da linguagem na prática II"

--- src/br/com/digitalinnovation/one/NovidadesJava10/InferenciaExemplo.java
+++ src/br/com/digitalinnovation/one/NovidadesJava10/InferenciaExemplo.java
@@ -3,20 +3,62 @@ package br.com.digitalinnovation.one.NovidadesJava10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.stream.Collectors;
 
 public class InferenciaExemplo {
+
+    //var não pode ser utilizado em variável global
+    //public var nome = "Joao";
+
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://docs.oracle.com/javase/10/language/");
-        URLConnection urlConnection = url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//        URL url = new URL("https://docs.oracle.com/javase/10/language/");
+//        URLConnection urlConnection = url.openConnection();
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+        printarNomeCompleto("Joao", "Santos");
+        printarSoma(5, 5, 5);
+
+    }
+
+    private static void connectAndPrintURLJavaOracle() throws IOException {
+        var url = new URL("https://docs.oracle.com/javase/10/language/");
+        var urlConnection = url.openConnection();
+        var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
         System.out.println(bufferedReader.lines()
             .collect(Collectors.joining())
             .replaceAll(">", ">\n"));
-
     }
+
+
+    public static void printarNomeCompleto(String nome, String sobrenome){
+        var nomeCompleto = String.format("%s %s", nome, sobrenome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static void printarSoma(int... numeros){
+        int soma;
+        if(numeros.length > 0){
+            soma = 0;
+            for(var numero: numeros){
+                soma += numero;
+            }
+
+            System.out.println("A soma é = " + soma);
+        }
+    }
+
+    /**********************Consegue***************************/
+
+    //variaveis locais inicializadas
+    //variavel suporte do enhanced for
+    //variavel suporte do for iterativo
+
+    /*******************Não Consegue**************************/
+    //var nao pode ser utilizado em nivel de classe;
+    //var nao pode ser utilizado como parametro
+    //var nao pode ser utilizado em variaveis locais nao inicializadas
 }
+
+
